526d2aef10e037d4f6f6fae037ed7093ae75a075
Mr.Viana <rafaelgov95@gmail.com>
2018-04-19 19:38:33 -0400

    up

--- Projeto-Spring/src/main/java/br/com/analise/compras/Entity/Cliente.java
+++ Projeto-Spring/src/main/java/br/com/analise/compras/Entity/Cliente.java
@@ -1,5 +1,9 @@
 package br.com.analise.compras.Entity;
 
+import br.com.analise.compras.Entity.enumeration.TipoClienteEnum;
+
+import java.util.Objects;
+
 public class Cliente {
 
     private Long id;
@@ -9,6 +13,59 @@ public class Cliente {
 
     private String cpfCnpj;
 
-//    private String  ;
+    private TipoClienteEnum tipo;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpfCnpj() {
+        return cpfCnpj;
+    }
+
+    public void setCpfCnpj(String cpfCnpj) {
+        this.cpfCnpj = cpfCnpj;
+    }
+
+    public TipoClienteEnum getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoClienteEnum tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return Objects.equals(id, cliente.id);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id);
+    }
 }
--- Projeto-Spring/src/main/java/br/com/analise/compras/Entity/enumeration/TipoClienteEnum.java
+++ Projeto-Spring/src/main/java/br/com/analise/compras/Entity/enumeration/TipoClienteEnum.java
@@ -1,4 +1,17 @@
 package br.com.analise.compras.Entity.enumeration;
 
 public enum TipoClienteEnum {
+
+   PESSOAFISICA("Pessoa Física"), PESSOAJURIDA("Pessoa Juridica");
+
+   String descricao;
+
+   private TipoClienteEnum(String descricao){
+       this.descricao = descricao;
+   }
+
+
+    public String getTipoCliente(){
+       return this.descricao;}
+
 }
